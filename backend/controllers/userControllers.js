30eb0ac0d81e28680ea3fb95364a0c19ee672cae
triminh12042002 <triminh12042002@gmail.com>
2023-01-02 13:03:50 +0700

    init UI file

--- backend/controllers/userControllers.js
+++ backend/controllers/userControllers.js
@@ -1,16 +1,70 @@
 const express = require('express')
+const bcrypt = require('bcryptjs')
+const asyncHandler = require('express-async-handler')
+const User = require('../models/userModel')
 
-const registerUser = (req, res) => {
-    res.status(201).json({
-        message: 'register successfully'
-    })
-}
 
-const loginUser = (req, res) => {
-    res.status(201).json({
-        message: 'register successfully'
+
+// @desc Register new user
+// @route POST api/users
+// @access public
+const registerUser = asyncHandler(async (req, res) => {
+    const {name, email, password} = req.body
+    console.log(req.body)
+
+    if(!email || !name || !password) {
+        res.status(400)
+        throw new Error('Please fill in all fields')    
+    }
+
+    const userExists = await User.findOne({email}) 
+    
+    if(userExists){
+        res.status(400)
+        throw new Error('User already exists')
+    }
+
+    const salt = await bcrypt.genSalt(9)
+    const hashedPassword = await bcrypt.hash(password, salt)
+
+    const user = await User.create({
+        name,
+        email,
+        password: hashedPassword
     })
-}
+
+    if(user){
+        res.status(200).json({
+            _id: user.id,
+            name: user.name,
+            email: user.email
+        })
+    } else {
+        res.status(400)
+        throw new Error('Invalid user data')
+    }
+})
+
+// @desc Login user
+// @route POST api/users/login
+// @access public
+const loginUser = asyncHandler(async (req, res) => {
+    const {email, password} = req.body
+
+    // check user email
+    const user = await User.findOne({email})
+
+    if(user && (await bcrypt.compare(password, user.password))){
+        res.json({
+            _id: user.id,
+            name: user.name,
+            email: user.email
+        })
+    } else {
+        res.status(400)
+        throw new Error('Invalid credentials')
+    }
+})
 
 
 const getMe = (req, res) => {
--- backend/server.js
+++ backend/server.js
@@ -1,8 +1,19 @@
 const express = require('express')
+const colors = require('colors')
+const connectDB = require('./config/db')
+const dotenv = require('dotenv').config()
 const app = express()
 const port = 5000
 
+connectDB()
+
+// to res with an json object
 app.use(express.json())
+
+// to req and pass data through req.body
+app.use(express.urlencoded({extends: false}))
+
+
 app.use('/api/users', require('./routes/userRoutes'))
 app.get('/', (req, res) => {
     res.status(200).json({message: 'Homepage'})
