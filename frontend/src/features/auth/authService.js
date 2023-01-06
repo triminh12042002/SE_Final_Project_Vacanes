9d2dc39667cb3f1360b0ce918d133537bb28f334
triminh12042002 <triminh12042002@gmail.com>
2023-01-06 20:11:28 +0700

    configPackage

---
+++ frontend/src/features/auth/authService.js
@@ -0,0 +1,20 @@
+import axios from 'axios'
+
+const API_URL = '/api/users/'
+
+// Login user
+const login = async (userData) => {
+    const response = await axios.post(API_URL + 'login', userData)
+
+    if (response.data) {
+        localStorage.setItem('user', JSON.stringify(response.data))
+    }
+
+    return response.data
+}
+
+const authService = {
+    login,
+}
+
+export default authService
